f9a785f0376705acfe828d0301e43b1cf0a50177
Sanskar Bajpai <sanskarbajpai2907@gmail.com>
2021-01-12 12:49:54 +0530

    Server: Added the full support for all the operations in the chat.
    This commit introduces the logic for al of the functions that the
    chat provides. An error has been removed from the client file.

--- chat/main.go
+++ chat/main.go
@@ -11,7 +11,7 @@ func main() {
 
 	listener, err := net.Listen("tcp", ":8888")
 	if err != nil {
-		log.Fatal("unable to start server: %s", err.Error())
+		log.Fatalf("unable to start server: %s", err.Error())
 	}
 
 	defer listener.Close()
